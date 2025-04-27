785910f3fdd99c02c514e751154d6d11eff91653
Albert-D17 <aldpega1013@gmail.com>
2025-04-27 18:17:10 -0500

    Create ControladorServicioDiario.java

---
+++ src/src/app/servicioDiario/controller/ControladorServicioDiario.java
@@ -0,0 +1,24 @@
+package src.app.servicioDiario.controller;
+
+import src.app.servicioDiario.service.ServicioServicioDiario;
+import src.middleware.Request;
+import src.middleware.Response;
+
+public class ControladorServicioDiario extends ServicioServicioDiario {
+
+    public void llamarActualizarServicioDiario(Request req, Response res) {
+        
+    }
+
+    public void llamarBorrarServicioDiario(Request req, Response res) {
+        
+    }
+
+    public void llamarObtenerTodosServiciosDiarios(Request req, Response res) {
+        
+    }
+
+    public void llamarGrabarServicioDiario(Request req, Response res) {
+        
+    }
+}
