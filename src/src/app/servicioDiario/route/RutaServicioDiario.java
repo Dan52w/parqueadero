d5684c02ce6d01eba16ab4de35e36445dcb2a038
Albert-D17 <aldpega1013@gmail.com>
2025-04-27 18:20:57 -0500

    Create RutaServicioDiario.java

---
+++ src/src/app/servicioDiario/route/RutaServicioDiario.java
@@ -0,0 +1,24 @@
+package src.app.servicioDiario.route;
+
+import java.util.List;
+import src.app.servicioDiario.model.ServicioDiario;
+
+public class RutaServicioDiario {
+    private ServicioDiario servicioDiarioService;
+
+    public List<ServicioDiario> obtenerTodos() {
+        return null;
+    }
+
+    public ServicioDiario crearServicioDiario(ServicioDiario servicioDiarioCrearDTO) {
+        return null;
+    }
+
+    public void borrarServicioDiario(int codServicioDiario) {
+        
+    }
+
+    public ServicioDiario actualizarServicioDiario(ServicioDiario servicioDiarioActualizarDTO) {
+        return null;
+    }
+}
