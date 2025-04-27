d4754c4f1c047f1f90c3553db19f523e6ff9fe9c
Dan <danflorezmartinez@gmail.com>
2025-04-27 16:56:19 -0500

    Creacion de rutas faltantes

---
+++ src/src/app/relTurnoUsuario/route/RutaRelTurnoUsuario.java
@@ -0,0 +1,21 @@
+package src.app.relTurnoUsuario.route;
+
+import java.util.List;
+import src.app.relTurnoUsuario.model.RelTurnoUsuario;
+
+public class RutaRelTurnoUsuario {
+    private RelTurnoUsuario rutaRelTurnoUsuarioApi;
+    
+    public List<RelTurnoUsuario> obtenerTodos(){
+        return null;
+    }
+    public void borrarRelTurnoUsuario(){
+        
+    }
+    public RelTurnoUsuario addRelTurnoUsuario(){
+        return null;
+    }
+    public RelTurnoUsuario actualizarRelTurnoUsuario(){
+        return null;
+    }
+}
---
+++ src/src/app/usuario/route/RutaUuario.java
@@ -0,0 +1,21 @@
+package src.app.usuario.route;
+
+import java.util.List;
+import src.app.usuario.model.Usuario;
+
+public class RutaUuario {
+    private Usuario rutaUsuarioApi;
+
+    public List<Usuario> obtenerTodos(){
+        return null;
+    }
+    public void borrarUsuario(){
+        
+    }
+    public Usuario actualizaarUsuario(){
+        return null;
+    }
+    public Usuario addUsuario(){
+        return null;
+    }
+}
