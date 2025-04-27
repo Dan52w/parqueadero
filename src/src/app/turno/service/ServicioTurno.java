7cd0e57b8ad0a1c2481202f6ea7e49f0f6452f95
Albert-D17 <aldpega1013@gmail.com>
2025-04-27 18:00:43 -0500

    Update ServicioTurno.java

--- src/src/app/turno/service/ServicioTurno.java
+++ src/src/app/turno/service/ServicioTurno.java
@@ -6,11 +6,11 @@ import src.app.turno.model.Turno;
 
 public class ServicioTurno {
 
-    public void actualizarTurno(Turno objTurno, ResponseCache res) {
+    public void actualizarTurno(Turno objTurno, Response Res) {
         
     }
 
-    public static void borrar(Turno objTurno, ResponseCache res) {
+    public static void borrar(Turno objTurno, Response res) {
         
     }
 
@@ -18,7 +18,7 @@ public class ServicioTurno {
         return null;
     }
 
-    public static void grabarTurno(Turno objTurno, ResponseCache res) {
+    public static void grabarTurno(Turno objTurno, Response res) {
         
     }
 }
