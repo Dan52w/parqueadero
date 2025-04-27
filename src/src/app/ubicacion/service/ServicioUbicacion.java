df4b2d8c783233895ca52e0222dec72b9d653034
Albert-D17 <aldpega1013@gmail.com>
2025-04-27 17:50:31 -0500

    Create ServicioUbicacion.java

---
+++ src/src/app/ubicacion/service/ServicioUbicacion.java
@@ -0,0 +1,24 @@
+package src.app.ubicacion.service;
+
+import java.util.List;
+import src.app.ubicacion.model.Ubicacion;
+import src.middleware.Response;
+
+public class ServicioUbicacion {
+
+    public void actualizarUbicacion(Ubicacion objUbicacion, Response res) {
+        
+    }
+
+    public static void borrar(Ubicacion objUbicacion, Response res) {
+        
+    }
+
+    public static List<Ubicacion> obtenerTodos() {
+        return null;
+    }
+
+    public static void grabarUbicacion(Ubicacion objUbicacion, Response res) {
+        
+    }
+}
