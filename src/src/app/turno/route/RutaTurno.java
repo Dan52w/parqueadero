6e01baa10eff4451b3a8e98ffc271824e7eec9ec
Albert-D17 <aldpega1013@gmail.com>
2025-04-27 17:43:04 -0500

    Create RutaTurno.java

---
+++ src/src/app/turno/route/RutaTurno.java
@@ -0,0 +1,24 @@
+package src.app.turno.route;
+
+import java.util.List;
+import src.app.turno.model.Turno;
+
+public class RutaTurno {
+    private Turno turnoService;
+
+    public List<Turno> obtenerTodos() {
+        return null;
+    }
+
+    public Turno crearTurno(Turno turnoCrearDTO) {
+        return null;
+    }
+
+    public void borrarTurno(int codTurno) {
+        
+    }
+
+    public Turno actualizarTurno(Turno turnoActualizarDTO) {
+        return null;
+    }
+}
