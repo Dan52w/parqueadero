30eceac595a25ed0b2a57484c08cff178060fc09
Albert-D17 <aldpega1013@gmail.com>
2025-04-27 17:52:56 -0500

    Create RutaUbicacion.java

---
+++ src/src/app/ubicacion/route/RutaUbicacion.java
@@ -0,0 +1,24 @@
+package src.app.ubicacion.route;
+
+import java.util.List;
+import src.app.ubicacion.model.Ubicacion;
+
+public class RutaUbicacion {
+    private Ubicacion ubicacionService;
+
+    public List<Ubicacion> obtenerTodas() {
+        return null;
+    }
+
+    public Ubicacion crearUbicacion(Ubicacion ubicacionCrearDTO) {
+        return null;
+    }
+
+    public void borrarUbicacion(int codUbicacion) {
+        
+    }
+
+    public Ubicacion actualizarUbicacion(Ubicacion ubicacionActualizarDTO) {
+        return null;
+    }
+}
