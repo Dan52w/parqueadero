e6d11983009caacd013d3e805985b34cccf830ea
Albert-D17 <aldpega1013@gmail.com>
2025-04-27 18:14:58 -0500

    Create ServicioServicioDiario.java

---
+++ src/src/app/servicioDiario/service/ServicioServicioDiario.java
@@ -0,0 +1,24 @@
+package src.app.servicioDiario.service;
+
+import java.util.List;
+import src.app.servicioDiario.model.ServicioDiario;
+import src.middleware.Response;
+
+public class ServicioServicioDiario {
+
+    public void actualizarServicioDiario(ServicioDiario objServicioDiario, Response res) {
+        
+    }
+
+    public static void borrar(ServicioDiario objServicioDiario, Response res) {
+        
+    }
+
+    public static List<ServicioDiario> obtenerTodos() {
+        return null;
+    }
+
+    public static void grabarServicioDiario(ServicioDiario objServicioDiario, Response res) {
+        
+    }
+}
