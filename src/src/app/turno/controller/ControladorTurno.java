e67cde3af9a93d9139cba213e2457ac77e058372
Albert-D17 <aldpega1013@gmail.com>
2025-04-27 17:37:37 -0500

    Create ControladorTurno.java

---
+++ src/src/app/turno/controller/ControladorTurno.java
@@ -0,0 +1,24 @@
+package src.app.turno.controller;
+
+import src.app.turno.service.ServicioTurno;
+import src.middleware.Request;
+import src.middleware.Response;
+
+public class ControladorTurno extends ServicioTurno {
+    
+    public void llamarActualizarTurno(Request req, Response res) {
+        
+    }
+
+    public void llamarBorrarTurno(Request req, Response res) {
+        
+    }
+
+    public void llamarObtenerTodosTurnos(Request req, Response res) {
+        
+    }
+
+    public void llamarGrabarTurno(Request req, Response res) {
+        
+    }
+}
