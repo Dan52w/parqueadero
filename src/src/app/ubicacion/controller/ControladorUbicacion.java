eafe4f734e8f041d9a35cb082b54f7c41fb6f71d
Albert-D17 <aldpega1013@gmail.com>
2025-04-27 17:51:40 -0500

    Create ControladorUbicacion.java

---
+++ src/src/app/ubicacion/controller/ControladorUbicacion.java
@@ -0,0 +1,24 @@
+package src.app.ubicacion.controller;
+
+import src.app.ubicacion.service.ServicioUbicacion;
+import src.middleware.Request;
+import src.middleware.Response;
+
+public class ControladorUbicacion extends ServicioUbicacion {
+    
+    public void llamarActualizarUbicacion(Request req, Response res) {
+        
+    }
+
+    public void llamarBorrarUbicacion(Request req, Response res) {
+        
+    }
+
+    public void llamarObtenerTodasUbicaciones(Request req, Response res) {
+        
+    }
+
+    public void llamarGrabarUbicacion(Request req, Response res) {
+        
+    }
+}
